80ca6cf4c5a97ffe09ffa6e5b4af70cdd5e92610
JonathaGomes <jonathagomes.alu.lmb@gmail.com>
2021-02-06 16:12:50 -0300

    First commit

---
+++ Readme.md
@@ -0,0 +1,37 @@
+<h1 align="center">
+  dev.finance$
+</h1>
+
+![preview da aplicação](.github/preview-light.png)
+
+![preview da aplicação](.github/preview-dark.png)
+
+<br>
+
+## 💻 Projeto
+<hr>
+
+<p>O projeto dev.finances é uma aplicação para um controle financeiro, onde é possível cadastrar e excluir transações e ver o saldo de entrada e saída</p>
+<br>
+
+## 🚀 Tecnologias
+<hr>
+
+* HTML
+* Css
+* JavaScript
+
+## 🔖 Layout
+<hr>
+
+Você pode visualizar o layout do projeto através desse [link](https://www.figma.com/file/7Vu9DzUaCZIV4nibzkjgB4/dev.finance%24-Maratona-Discover?node-id=0%3A1).
+
+## 📝 Licença
+<hr>
+
+Esse projeto está sob a licença MIT. Veja o arquivo [License](License.md) para mais detalhes.
+<hr>
+
+Feito com 💗 by Jonatha com a ajuda da [Rocketseat](https://github.com/Rocketseat)🚀
+
+Entre para nossa [Comunidade](https://discord.gg/VmuPXy2E)🚀
---
+++ assets/js/darkMode.js
@@ -0,0 +1,33 @@
+const html = document.querySelector("html");
+const checkbox = document.querySelector("input[name=theme]");
+
+const getStyle = (element, style) =>
+  window.getComputedStyle(element).getPropertyValue(style);
+
+const initialColors = {
+  bg: getStyle(html, "--bg"),
+  lightGray: getStyle(html, "--light-gray"),
+  bgHeader: getStyle(html, "--bg-header"),
+  colorFooter: getStyle(html, "--color-footer"),
+};
+//
+
+const darkMode = {
+  bg: "#333",
+  lightGray: "#303234",
+  bgHeader: "#6B0FC1",
+  colorFooter: "#fff",
+};
+
+const transformKey = (key) =>
+  "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
+
+const changeColors = (colors) => {
+  Object.keys(colors).map((key) =>
+    html.style.setProperty(transformKey(key), colors[key])
+  );
+};
+
+checkbox.addEventListener("change", ({ target }) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialColors);
+});
